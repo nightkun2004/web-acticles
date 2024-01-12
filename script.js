29b71e830192616a4c9e1ad05cb9bea2656dcff4
Nightkun <nthe7610@gmail.com>
2024-01-12 20:42:27 +0700

    commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('navbar-toggle').addEventListener('click', function () {
+    document.querySelector('.navbar-menu').classList.toggle('show');
+    document.querySelector('.container-bars').classList.toggle('change');
+});
